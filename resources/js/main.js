5e5031915236cdef6fbd43d4c396be6e62fc98dd
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-05-23 02:13:47 +0100

    Scroll Reveal Animation

--- resources/js/main.js
+++ resources/js/main.js
@@ -97,3 +97,14 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    //reset: true,
+})
+
+sr.reveal('.home_data')
+sr.reveal('.home_handle', {delay: 700})
+sr.reveal('.home_social, home_scroll', {delay: 900, origin: 'bottom'})
