f8281c5f895c1adaf891479e1257d2429a6619db
Stanislava Stoeva <stanislavastoeva1410@gmail.com>
2022-12-03 21:56:41 +0000

    change some styling

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,31 +2,31 @@
 "use strict";
 
 function scrollHeader(){
-    const header = document.getElementById('header')
+    const header = document.getElementById('header');
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header');
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener('scroll', scrollHeader);
 
 /*=============== QUALIFICATION ===============*/
 const tabs = document.querySelectorAll('[data-target]'),
-    tabContent = document.querySelectorAll('[data-content]')
+    tabContent = document.querySelectorAll('[data-content]');
 
 tabs.forEach(tab =>{
     tab.addEventListener('click', () =>{
-        const target = document.querySelector(tab.dataset.target)
+        const target = document.querySelector(tab.dataset.target);
 
         tabContent.forEach(tabContent =>{
-            tabContent.classList.remove('qualification_active')
-        })
-        target.classList.add('qualification_active')
+            tabContent.classList.remove('qualification_active');
+        });
+        target.classList.add('qualification_active');
 
         tab.forEach(tab =>{
-            tab.classList.remove('qualification_active')
-        })
-        tab.classList.add('qualification_active')
-    })
-})
+            tab.classList.remove('qualification_active');
+        });
+        tab.classList.add('qualification_active');
+    });
+});
 
 /*=============== PORTFOLIO ===============*/
 let swiper = new Swiper(".portfolio_container", {
@@ -42,55 +42,54 @@ let swiper = new Swiper(".portfolio_container", {
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll('section[id]');
 
 function scrollActive(){
-    const scrollY = window.pageYOffset
+    const scrollY = window.pageYOffset;
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
               sectionTop = current.offsetTop - 58,
-              sectionId = current.getAttribute('id')
+              sectionId = current.getAttribute('id');
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link')
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active-link');
         }else{
-            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link')
+            document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active-link');
         }
-    })
+    });
 }
-window.addEventListener('scroll', scrollActive)
+window.addEventListener('scroll', scrollActive);
 
 /*=============== LIGHT DARK THEME ===============*/ 
-const themeButton = document.getElementById('theme-button')
-const lightTheme = 'light-theme'
-const iconTheme = 'ri-sun-line'
+const themeButton = document.getElementById('theme-button');
+const lightTheme = 'light-theme';
+const iconTheme = 'ri-sun-line';
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
 // We obtain the current theme that the interface has by validating the light-theme class
-const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line';
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the light
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
-  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme);
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
 }
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
     // Add or remove the light / icon theme
-    document.body.classList.toggle(lightTheme)
-    themeButton.classList.toggle(iconTheme)
+    document.body.classList.toggle(lightTheme);
+    themeButton.classList.toggle(iconTheme);
     // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
-
+    localStorage.setItem('selected-theme', getCurrentTheme());
+    localStorage.setItem('selected-icon', getCurrentIcon());
+});
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
     origin: 'top',
@@ -98,8 +97,8 @@ const sr = ScrollReveal({
     duration: 2500,
     delay: 400,
     //reset: true,
-})
+});
 
-sr.reveal('.home_data')
-sr.reveal('.home_handle', {delay: 700})
-sr.reveal('.home_social, home_scroll', {delay: 900, origin: 'bottom'})
+sr.reveal('.home_data');
+sr.reveal('.home_handle', {delay: 700});
+sr.reveal('.home_social, home_scroll', {delay: 900, origin: 'bottom'});
